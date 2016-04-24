b606c727240fa70800be3eed0265e8ef07f2015e
Micha Gorelick <mynameisfiber@gmail.com>
2016-04-24 12:57:23 -0600

    added run script

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+port=7070
+pid=$(lsof -t -i:$port)
+if [[ -z $pid ]]
+   then
+      echo "The game was not running."
+   else
+      kill -9 $pid
+fi
+
+cd dist
+python3 -m http.server $port
+
